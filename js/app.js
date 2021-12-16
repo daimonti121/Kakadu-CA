f83653a3bcde7361f87c31baeefcc83def360315
daimonti121 <damios.1812@gmail.com>
2021-12-16 09:13:23 +0300

    3

--- js/app.js
+++ js/app.js
@@ -208,9 +208,16 @@ $('.btn-1').click(function(){
 
 })
 
-$('.btn-2').click(function(){
-  window.location.href = 'https://www.casinokakadu.com/users/sign_up';
+function $_GET(e,t){return!!(t=t.match(new RegExp(e+"=([^&=]+)")))&&t[1]}
+var getStag = $_GET("stag", window.location.href);
+var stag = "?stag=" + getStag;
+console.log("STAG -" + stag);
+
+
+$('.btn-2').click(function(){     
+    window.location.href = 'https://www.casinokakadu.com/en-CA/users/sign_up' + stag;
 })
+
 
 function easeOut(t, b, c, d) {
   var ts = (t/=d)*t;
